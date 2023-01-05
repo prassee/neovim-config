0312e8d8504defab9b22e89afa3d889405168273
Prasanna Kumar <prasanna.kumar@lendingkart.com>
2023-01-05 11:07:44 +0530

    short cut to invoke lazygit & terminal

--- lua/core/plugin_config/toggleterm.lua
+++ lua/core/plugin_config/toggleterm.lua
@@ -5,4 +5,5 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true })

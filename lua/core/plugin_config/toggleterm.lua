28c833db2e322dfc332a4b942e59b536f0af126d
Prasanna Kumar <prasanna.kumar@lendingkart.com>
2023-01-07 14:25:06 +0530

    keymap for toggle term

--- lua/core/plugin_config/toggleterm.lua
+++ lua/core/plugin_config/toggleterm.lua
@@ -1,9 +1,13 @@
 require("toggleterm").setup {}
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true , direction = "float"})
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 function _lazygit_toggle()
   lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>|", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>_", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader><leader><Enter>", ":ToggleTermSendVisualSelection direction=vertical<CR>",
+  { noremap = true, silent = true })

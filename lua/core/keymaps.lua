f6c676a5d362ba4e37d224e985747a89771a5348
Prasanna Kumar <prasanna.kumar@lendingkart.com>
2023-01-18 21:58:11 +0530

    jump to start & end of line

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,7 +9,7 @@ vim.keymap.set(
   require('telescope').extensions.file_browser.file_browser,
   { noremap = true }
 )
-vim.keymap.set('i','<C-a>','<Esc>I')
-vim.keymap.set('n','<C-e>','<Esc>$')
-vim.keymap.set('i','<C-e>','<Esc>A')
-vim.keymap.set('n','<C-a>','<Esc>^')
+vim.keymap.set('i','<leader>a','<Esc>g0')
+vim.keymap.set('n','<leader>e','<Esc>$')
+vim.keymap.set('i','<leader>e','<Esc>g0')
+vim.keymap.set('n','<leader>a','<Esc>^')

dc291cd22ab489437335e824cf07cc5be5638b1f
Prasanna <Prasanna>
2023-12-17 20:18:26 +0530

    removed un-used variable

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
+-- local opts = {}
 
 require("vim-options")
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } })

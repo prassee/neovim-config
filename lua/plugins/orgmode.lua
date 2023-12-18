5756e1dd0bb501b7d81994084fce5012031ad97d
Prasanna <Prasanna>
2023-12-18 08:53:02 +0530

    updated org-capture config

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -18,7 +18,14 @@ return {
     })
 
     -- Setup orgmode
+    -- I'm classifying this as LRDE i.e L -> Learning , R -> Research , D -> Develop, E -> Errands
     require('orgmode').setup({
+      org_capture_templates = {
+        l = { description = 'Learning - All Learnings', template = '* TODO %?\n  %u' },
+        r = { description = 'Research - Research and observations for everything', template = '* TODO %?\n  %u' },
+        d = { description = 'Develop - Write some code', template = '* TODO %?\n  %u' },
+        e = { description = 'Errands - All type of home tasks', template = '* TODO %?\n %u' }
+      },
       org_agenda_files = '/data/org/**/*',
       org_default_notes_file = '/data/org/refile.org',
     })

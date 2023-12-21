28950d3a819d356792a8058906bf75caedf0615e
Prasanna <Prasanna>
2023-12-21 08:49:01 +0530

    configured org agenda files

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -21,16 +21,41 @@ return {
     -- I'm classifying this as LRDE i.e L -> Learning , R -> Research , D -> Develop, E -> Errands
     require('orgmode').setup({
       org_capture_templates = {
-        b = { description = 'Blogging - All Blogging', template = '* %^{PROMPT} %?\n  %u' },
-        l = { description = 'Learning - All Learnings', template = '* %^{PROMPT} %?\n  %u' },
-        r = { description = 'Research - Research and observations for everything', template = '* TODO %?\n  %u' },
-        d = { description = 'Develop - Write some code', template = '* TODO %?\n  %u' },
-        e = { description = 'Errands - All type of home tasks', template = '* TODO %?\n %u' }
+        b = {
+          description = 'Blogging - All Blogging',
+          template = '* %^{Blog Title} %?  \n \t %u',
+          target = '~/org/blogging.org'
+        },
+        l = {
+          description = 'Learning - All Learnings',
+          template = '* %^{What to learn ? } %?  \n \t  %u',
+          target = '~/org/learning.org'
+        },
+        r = {
+          description = 'Research - Research and observations for everything',
+          template = '* %^{Which to research on ?} %? \t \n \t %u',
+          target = '~/org/research.org'
+        },
+        d = {
+          description = 'Develop - Write some code',
+          template = '* %^{What to code ?} %?\n \t %u',
+          target = '~/org/develop.org'
+        },
+        e = {
+          description = 'Errands - All type of home tasks',
+          template = '* %^{What task todo ?} %?\n \t %u',
+          target = '~/org/errands.org'
+        }
       },
       org_todo_keywords = { 'TODO(t)', 'WAITING(w)', '|', 'DONE(d)', 'DELEGATED(g)' },
       win_split_mode = 'float',
       org_agenda_files = '/data/org/**/*',
       org_default_notes_file = '/data/org/refile.org',
+      mappings = {
+        global = {
+          org_toggle_checkbox = '<C-S-Space>'
+        }
+      }
     })
   end,
 }

d824de8e8cd22fc6a0010abedf5da15725ff638b
Prasanna <Prasanna>
2023-12-23 06:55:43 +0530

    added lazygit shortcut

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,14 +5,27 @@ return {
     require("toggleterm").setup()
     local Terminal = require('toggleterm.terminal').Terminal
     local Taskui   = Terminal:new({ cmd = "twtui", hidden = true, direction = "float" })
+    local Lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
     function _taskui_toggle()
       Taskui:toggle()
     end
 
-    vim.api.nvim_set_keymap("n", "<leader>tw", "<cmd>lua _taskui_toggle()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>|", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>_", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
+    function _lazygit_toggle()
+      Lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>tw", "<cmd>lua _taskui_toggle()<CR>",
+      { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>",
+      { noremap = true, silent = true })
+
+    vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>",
+      { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>|", ":ToggleTerm direction=vertical<CR>",
+      { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>_", ":ToggleTerm direction=horizontal<CR>",
+      { noremap = true, silent = true })
     vim.api.nvim_set_keymap("v", "<leader><leader><Enter>", ":ToggleTermSendVisualSelection <CR>",
       { noremap = true, silent = true })
   end

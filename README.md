e4018dad6798db56d25e59a8a7e75f7cb82c2ce5
Prasanna <Prasanna>
2023-12-27 09:01:07 +0530

    configured markdown & updated readme

---
+++ README.md
@@ -0,0 +1,19 @@
+## Prassee Neovim Config
+
+### Features
+
+- Lazy
+- LSP 
+    - Rust
+    - Lua
+    - Scala
+    - Python
+    - Julia
+- Custom Keymaps - inherited from VSCode and my previous editors
+- Markdown previews
+
+### Tech Stack 
+- Neovim 
+- TaskWarrior
+- LazyGit
+- Markdown 
--- lua/plugins/markdown-support.lua
+++ lua/plugins/markdown-support.lua
@@ -3,7 +3,13 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
     init = function()
+        vim.g.mkdp_auto_start = 1
+        vim.g.mkdp_auto_close = 1
         vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_browser = "/usr/bin/firefox"
+        vim.g.mkdp_combine_preview = 0
+        vim.g.mkdp_combine_preview_auto_refresh = 1
+        vim.g.mkdp_port = "8999"
     end,
     ft = { "markdown" },
 }

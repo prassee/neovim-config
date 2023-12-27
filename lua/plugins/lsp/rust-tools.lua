f4ba3fadbe8b30fa0148380970ab0e4fe0f1e276
Prasanna <Prasanna>
2023-12-28 05:11:15 +0530

    code format

--- lua/plugins/lsp/rust-tools.lua
+++ lua/plugins/lsp/rust-tools.lua
@@ -34,8 +34,9 @@ return {
                         vim.lsp.buf.format({ async = true })
                     end, opts)
 
+                    -- go to declaration
                     opts.desc = "Go to declaration"
-                    keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+                    keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
                     opts.desc = "Show LSP definitions"
                     keymap.set(
@@ -73,44 +74,51 @@ return {
                         opts
                     )
 
+                    -- see available code actions, in visual mode will apply to selection
                     opts.desc = "See available code actions"
                     keymap.set(
                         { "n", "v" },
                         "<leader>ca",
                         vim.lsp.buf.code_action,
                         opts
-                    ) -- see available code actions, in visual mode will apply to selection
+                    )
 
                     opts.desc = "Smart rename"
                     keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
+                    -- show  diagnostics for file
                     opts.desc = "Show buffer diagnostics"
                     keymap.set(
                         "n",
                         "<leader>D",
                         "<cmd>Telescope diagnostics bufnr=0<CR>",
                         opts
-                    ) -- show  diagnostics for file
+                    )
 
+                    -- show diagnostics for line
                     opts.desc = "Show line diagnostics"
                     keymap.set(
                         "n",
                         "<leader>d",
                         vim.diagnostic.open_float,
                         opts
-                    ) -- show diagnostics for line
+                    )
 
+                    -- jump to previous diagnostic in buffer
                     opts.desc = "Go to previous diagnostic"
-                    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+                    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
+                    -- jump to next diagnostic in buffer
                     opts.desc = "Go to next diagnostic"
-                    keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+                    keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
+                    -- show documentation for what is under cursor
                     opts.desc = "Show documentation for what is under cursor"
-                    keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+                    keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
+                    -- mapping to restart lsp if necessary
                     opts.desc = "Restart LSP"
-                    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+                    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
                 end,
             },
             tools = {

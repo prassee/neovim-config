de23e74733a3cb8984e1bbd1005b4662bd8eb942
Prasanna <Prasanna>
2024-01-01 13:10:38 +0530

    updated options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,27 +12,22 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.shell = "fish"
 vim.opt.showcmd = true
-vim.wo.number = true
 vim.opt.smarttab = true
+vim.opt.scrolloff = 10
+vim.opt.colorcolumn = "90"
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.wo.number = true
 
 vim.g.maplocalleader = "\\"
-
--- vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
 vim.wo.number = true
--- Save undo history
--- vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-
--- Keep signcolumn on by default
 vim.wo.signcolumn = "yes"
 
--- Decrease update time
--- vim.o.updatetime = 250
--- vim.o.timeoutlen = 300
+-- Case-insensitive searching UNLESS \C or capital in search
 
 vim.keymap.set({ "n", "i" }, "<leader>s", "<Esc>:w<cr>")
 

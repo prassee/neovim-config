2b4e8e6271090659ed4ecd2c5b15786cf459ae31
Prasanna <Prasanna>
2024-01-13 05:20:17 +0530

    fixed level for new agenda items

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -41,42 +41,42 @@ return {
                 org_capture_templates = {
                     b = {
                         description = "Blogging - All Blogging",
-                        template = "** %^{Blog Title} %?  \n \t %u",
+                        template = "\n* %^{Blog Title} %?  \n \t %u",
                         target = "~/org/blogging.org",
                     },
                     l = {
                         description = "Learning - All Learnings",
-                        template = "** %^{What to learn ? } %?  \n \t  %u",
+                        template = "\n* %^{What to learn ? } %?  \n \t  %u",
                         target = "~/org/learning.org",
                     },
                     r = {
                         description = "Research - Research and observations for everything",
-                        template = "** %^{Which to research on ?} %? \t \n \t %u",
+                        template = "\n* %^{Which to research on ?} %? \t \n \t %u",
                         target = "~/org/research.org",
                     },
                     h = {
                         description = "Habbit Tracker - Anything specific to test",
-                        template = "** %^{what to track} %? \t \n \t %u",
+                        template = "\n* %^{what to track} %? \t \n \t %u",
                         target = "~/org/habbit.org",
                     },
                     s = {
                         description = "Shopping / Bucket List",
-                        template = "** %^{What to buy / do } %? \t \n \t %u",
+                        template = "\n* %^{What to buy / do } %? \t \n \t %u",
                         target = "~/org/bucketlist.org",
                     },
                     d = {
                         description = "Develop - Write some code",
-                        template = "** %^{What to code ?} %?\n \t %u",
+                        template = "\n* %^{What to code ?} %?\n \t %u",
                         target = "~/org/develop.org",
                     },
                     e = {
                         description = "Errands - All type of home tasks",
-                        template = "** %^{What task todo ?} %?\n \t %u",
+                        template = "\n* %^{What task todo ?} %?\n \t %u",
                         target = "~/org/errands.org",
                     },
                     u = {
                         description = "Link to refer to",
-                        template = "** %^{Link} %?\n \t %u",
+                        template = "\n* %^{Link} %?\n \t %u",
                         target = "~/org/links.org",
                     },
                 },

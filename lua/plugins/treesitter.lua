a6536ab41494f59414667b662d2527682d1e9855
Prasanna <Prasanna>
2024-01-13 12:38:19 +0530

    removed un-used lines

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,18 +4,6 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            -- ensure_installed = {
-            --     "lua",
-            --     "javascript",
-            --     "typescript",
-            --     "rust",
-            --     "toml",
-            --     "scala",
-            --     "julia",
-            --     "org",
-            --     "markdown",
-            --     "java",
-            -- },
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },

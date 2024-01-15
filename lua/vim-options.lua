fd8ff5c5d7653ba360255d471f9731ff8630f91c
Prasanna <Prasanna>
2024-01-15 09:14:41 +0530

    updated clipboard settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
--- vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("set clipboard=unnamedplus")
 
 vim.opt.cmdheight = 1
 vim.opt.termguicolors = true
